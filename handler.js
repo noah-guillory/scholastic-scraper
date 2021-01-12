fb70cd91398ebbc3b5327aa4b9d2dd1998c03694
Noah Guillory <noah@noahguillory.me>
2021-01-12 00:26:53 -0600

    serverless stuff done

--- handler.js
+++ handler.js
@@ -1,23 +1,56 @@
-const puppeteer = require('puppeteer');
-const { getChrome } = require('./chrome-script');
-const { getAllBooks, writeLevelsToSheet } = require('./sheets')
+const chromium = require('chrome-aws-lambda');
+const googleSheets = require('./sheets')
+const scraper = require('./scraper');
+const AWS = require('aws-sdk');
 
+const lambda = new AWS.Lambda({
+  region: 'us-east-1',
+});
 
-module.exports.getGuidedReadingLevel = async (event) => {
-  const { title } = event.queryStringParameters;
-  const chrome = await getChrome();
-  const browser = await puppeteer.connect({
-    browserWSEndpoint: chrome.endpoint,
+module.exports.startScrapes = async (event) => {
+  const sheets = await googleSheets();
+
+  const books = await sheets.getAllBooks();
+
+  books.forEach(book => {
+    const params = {
+      FunctionName: 'scholastic-scraper-dev-get_guided_reading_level',
+      InvocationType: 'RequestResponse',
+      Payload: JSON.stringify(book)
+    };
+
+    return lambda.invoke(params, (error, data) => {
+      if (error) {
+        console.error(JSON.stringify(error))
+        return new Error(`Error printing messages: ${JSON.stringify(error)}`);
+      } else if (data) {
+        console.log(data);
+      }
+    })
+  })
+}
+
+module.exports.getGuidedReadingLevel = async (book) => {
+  const sheets = await googleSheets();
+
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
   });
 
-  
   const page = await browser.newPage();
-  await page.goto(url, { waitUntil: 'networkidle0' });
-  const content = await page.evaluate(() => document.body.innerHTML);
+
+  const bookWithLevel = await scraper.getGuidedReadingLevel(book, page);
+
+  await sheets.writeLevelToSheet(bookWithLevel);
+ 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      content,
+      bookWithLevel,
     }),
   };
 };
--- sheets.js
+++ sheets.js
@@ -1,62 +1,66 @@
 const { GoogleSpreadsheet } = require("google-spreadsheet");
 
-
-const getAllBooks = async () => {
-  const doc = new GoogleSpreadsheet(process.env.SHEET_ID);
-
-  await doc.useServiceAccountAuth({
-    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    private_key: JSON.parse(process.env.GOOGLE_PRIVATE_KEY),
-  });
-
-  await doc.loadInfo(); // loads document properties and worksheets
-
-  const sheet = doc.sheetsByTitle[process.env.SHEET_NAME];
-
-  const bookRows = await sheet.getRows();
-
-  return bookRows
-  .filter(row => row.Level !== '-' && row.Level === undefined)
-  .map(row => ({
-    title: row.Title,
-    rowNumber: row.rowNumber,
-  }));
-
-};
-
 const delay = interval => new Promise(resolve => setTimeout(resolve, interval));
 
 
-const writeLevelsToSheet = async (books) => {
+const init = async () => {
   const doc = new GoogleSpreadsheet(process.env.SHEET_ID);
 
   await doc.useServiceAccountAuth({
     client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    private_key: JSON.parse(process.env.GOOGLE_PRIVATE_KEY),
+    private_key: process.env.GOOGLE_PRIVATE_KEY,
   });
 
   await doc.loadInfo(); // loads document properties and worksheets
 
   const sheet = doc.sheetsByTitle[process.env.SHEET_NAME];
 
-  const bookRows = await sheet.getRows();
-
-  console.log('Writing ' + books.length + ' levels');
 
 
-  for (const book of books) {
-    const matchingRow = bookRows.find(sheetRow => book.rowNumber === sheetRow.rowNumber);
-    if (matchingRow) {
-      console.log('Found matching for ' + book.title);
-      matchingRow.Level = book.level ? book.level : '-',
-      await matchingRow.save();
-    }
-    await delay(1000);
+  const getAllBooks = async () => {
+    const bookRows = await sheet.getRows();
+
+    return bookRows
+    .filter(row => row.Level !== '-' && row.Level === undefined)
+    .map(row => ({
+      title: row.Title,
+      rowNumber: row.rowNumber,
+    }));
   }
 
+  const writeLevelToSheet = async book => {
+    const { rowNumber, level } = book;
+    
+    const levelCell = await sheet.getCell(rowNumber - 1, 5);
+    levelCell.value = level ? level : '-';
+    await levelCell.save();
+  }
+
+  const writeLevelsToSheet = async books => {
+    console.log('Writing ' + books.length + ' levels');
+  
+  
+    for (const book of books) {
+      await writeLevelToSheet(book);
+      await delay(1000);
+    }
+  }
+
+  const getTitle = async rowNumber => {
+    const titleCell = await sheet.getCell(rowNumber - 1, 0);
+    return titleCell.value;
+  }
+
+  return { 
+    getAllBooks,
+    writeLevelsToSheet,
+    writeLevelToSheet,
+    getTitle,
+  }
 }
 
-module.exports = {
-  getAllBooks,
-  writeLevelsToSheet,
-};
+
+
+
+
+module.exports = init;
